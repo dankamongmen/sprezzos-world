eb460bcdc47679888f538d7be855fc94e5b489a4
nick black <dank@qemfd.net>
2012-06-21 03:58:41 -0400

    growlight packaging

--- Makefile
+++ Makefile
@@ -1,8 +1,12 @@
 .DELETE_ON_ERROR:
 .PHONY: world all clean
 
-world:
+world: omphalos
 
 all: world
+
+.PHONY: omphalos
+omphalos:
+	wget http://dank.qemfd.net/pub/growlight-0.0.99-pre.tar.bz2
 
 clean:
---
+++ growlight/growlight.default.ex
@@ -0,0 +1,10 @@
+# Defaults for growlight initscript
+# sourced by /etc/init.d/growlight
+# installed at /etc/default/growlight by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
