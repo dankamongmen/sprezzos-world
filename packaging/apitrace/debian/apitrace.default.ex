44263e7aa839e291666ec6926622d5b71e7d61c3
nick black <nick.black@sprezzatech.com>
2012-08-18 03:41:58 -0400

    apitrace packaging. resolves bug 278

---
+++ packaging/apitrace/debian/apitrace.default.ex
@@ -0,0 +1,10 @@
+# Defaults for apitrace initscript
+# sourced by /etc/init.d/apitrace
+# installed at /etc/default/apitrace by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ packaging/apitrace/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(apitrace):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="apitrace" command="/usr/bin/apitrace"
