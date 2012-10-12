e4eef1ed9a72efe9c6b6d21575ea94ba251daacc
nick black <nick.black@sprezzatech.com>
2012-10-12 16:07:10 -0400

    mutter

---
+++ packaging/mutter/debian/tmp/usr/include/mutter/meta/keybindings.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright (C) 2009 Intel Corporation.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef META_KEYBINDINGS_H
+#define META_KEYBINDINGS_H
+
+#include <meta/display.h>
+#include <meta/common.h>
+
+#define META_TYPE_KEY_BINDING               (meta_key_binding_get_type ())
+
+const char          *meta_key_binding_get_name      (MetaKeyBinding *binding);
+MetaVirtualModifier  meta_key_binding_get_modifiers (MetaKeyBinding *binding);
+guint                meta_key_binding_get_mask      (MetaKeyBinding *binding);
+
+gboolean meta_keybindings_set_custom_handler (const gchar        *name,
+					      MetaKeyHandlerFunc  handler,
+					      gpointer            user_data,
+					      GDestroyNotify      free_data);
+
+void meta_keybindings_switch_window (MetaDisplay    *display,
+				     MetaScreen     *screen,
+				     MetaWindow     *event_window,
+				     XEvent         *event,
+				     MetaKeyBinding *binding);
+
+
+void     meta_screen_ungrab_all_keys (MetaScreen *screen, guint32 timestamp);
+gboolean meta_screen_grab_all_keys (MetaScreen *screen, guint32 timestamp);
+#endif
---
+++ packaging/mutter/debian/tmp/usr/include/mutter/meta/meta-shaped-texture.h
@@ -0,0 +1,87 @@
+/*
+ * shaped texture
+ *
+ * An actor to draw a texture clipped to a list of rectangles
+ *
+ * Authored By Neil Roberts  <neil@linux.intel.com>
+ *
+ * Copyright (C) 2008 Intel Corporation
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+ * 02111-1307, USA.
+ */
+
+#ifndef __META_SHAPED_TEXTURE_H__
+#define __META_SHAPED_TEXTURE_H__
+
+#include <clutter/clutter.h>
+#include <X11/Xlib.h>
+
+G_BEGIN_DECLS
+
+#define META_TYPE_SHAPED_TEXTURE            (meta_shaped_texture_get_type())
+#define META_SHAPED_TEXTURE(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj),META_TYPE_SHAPED_TEXTURE, MetaShapedTexture))
+#define META_SHAPED_TEXTURE_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), META_TYPE_SHAPED_TEXTURE, MetaShapedTextureClass))
+#define META_IS_SHAPED_TEXTURE(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), META_TYPE_SHAPED_TEXTURE))
+#define META_IS_SHAPED_TEXTURE_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), META_TYPE_SHAPED_TEXTURE))
+#define META_SHAPED_TEXTURE_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), META_TYPE_SHAPED_TEXTURE, MetaShapedTextureClass))
+
+typedef struct _MetaShapedTexture        MetaShapedTexture;
+typedef struct _MetaShapedTextureClass   MetaShapedTextureClass;
+typedef struct _MetaShapedTexturePrivate MetaShapedTexturePrivate;
+
+struct _MetaShapedTextureClass
+{
+  ClutterActorClass parent_class;
+};
+
+struct _MetaShapedTexture
+{
+  ClutterActor parent;
+
+  MetaShapedTexturePrivate *priv;
+};
+
+GType meta_shaped_texture_get_type (void) G_GNUC_CONST;
+
+ClutterActor *meta_shaped_texture_new (void);
+
+void meta_shaped_texture_set_create_mipmaps (MetaShapedTexture *stex,
+					     gboolean           create_mipmaps);
+
+void meta_shaped_texture_update_area (MetaShapedTexture *stex,
+                                      int                x,
+                                      int                y,
+                                      int                width,
+                                      int                height);
+
+void meta_shaped_texture_set_pixmap (MetaShapedTexture *stex,
+                                     Pixmap             pixmap);
+
+CoglHandle meta_shaped_texture_get_texture (MetaShapedTexture *stex);
+
+void meta_shaped_texture_set_mask_texture (MetaShapedTexture *stex,
+                                           CoglHandle         mask_texture);
+
+/* Assumes ownership of clip_region */
+void meta_shaped_texture_set_clip_region (MetaShapedTexture *stex,
+					  cairo_region_t    *clip_region);
+
+cairo_surface_t * meta_shaped_texture_get_image (MetaShapedTexture     *stex,
+                                                 cairo_rectangle_int_t *clip);
+
+G_END_DECLS
+
+#endif /* __META_SHAPED_TEXTURE_H__ */
