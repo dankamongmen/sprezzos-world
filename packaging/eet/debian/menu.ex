5e04021d7433b78ddabcc6adceaa5dfa7434177d
nick black <nick.black@sprezzatech.com>
2012-11-09 18:14:14 -0500

    add eet recipe and import it

---
+++ packaging/eet/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(libeina):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="libeina" command="/usr/bin/libeina"
---
+++ packaging/eet/debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+liblibeina 1.7.1 libeina (>> 1.7.1-0), libeina (<< 1.7.1-99)
