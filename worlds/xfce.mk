70794184c5dfc5cb6407689bfd439d4193260d7d
nick black <nick.black@sprezzatech.com>
2012-12-20 06:29:46 -0500

    move xfce4 recipes into worlds/xfce.mk

---
+++ worlds/xfce.mk
@@ -0,0 +1,40 @@
+.PHONY: thunar
+thunar:$(THUNAR)_$(ARCH).deb
+$(THUNAR): $(SPREZZ)/thunar/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf Thunar-$(thunar_UPVER).tar.bz2 $(TARARGS) $@
+
+.PHONY: xfconf
+xfconf:$(XFCONF)_$(ARCH).deb
+$(XFCONF): $(SPREZZ)/xfconf/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf xfconf-$(xfconf_UPVER).tar.bz2 $(TARARGS) $@
+
+.PHONY: libxfce4ui
+libxfce4ui:$(LIBXFCE4UI)_$(ARCH).deb
+$(LIBXFCE4UI): $(SPREZZ)/libxfce4ui/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf libxfce4ui-$(libxfce4ui_UPVER).tar.bz2 $(TARARGS) $@
+
+.PHONY: libxfce4util
+libxfce4util:$(LIBXFCE4UTIL)_$(ARCH).deb
+$(LIBXFCE4UTIL): $(SPREZZ)/libxfce4util/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf libxfce4util-$(libxfce4util_UPVER).tar.bz2 $(TARARGS) $@
+
+.PHONY: xfce4-terminal
+xfce4-terminal:$(XFCE4TERMINAL)_$(ARCH).deb
+$(XFCE4TERMINAL): $(SPREZZ)/xfce4-terminal/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf Terminal-$(xfce4-terminal_UPVER).tar.bz2 $(TARARGS) $@
+
