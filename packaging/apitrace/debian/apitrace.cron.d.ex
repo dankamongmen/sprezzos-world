19d2146a6d6557fc879d0fc5ff54b767e6adda91
nick black <nick.black@sprezzatech.com>
2012-12-27 07:59:43 -0500

    clean lots of cruft out of apitrace

--- packaging/apitrace/debian/apitrace.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the apitrace package
-#
-0 4	* * *	root	[ -x /usr/bin/apitrace_maintenance ] && /usr/bin/apitrace_maintenance
--- packaging/apitrace/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(apitrace):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="apitrace" command="/usr/bin/apitrace"
