ec85e499474e6b85d32eb24193e0bbcd2b042c97
nick black <nick.black@sprezzatech.com>
2012-12-29 04:40:08 -0500

    recipe for kdeartwork

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -1,3 +1,11 @@
+.PHONY: kdeartwork
+kdeartwork:$(KDEARTWORK)_$(ARCH).deb
+$(KDEARTWORK): $(SPREZZ)/kdeartwork/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf kdeartwork_$(kdeartwork_UPVER).orig.tar.xz $(TARARGS) $@
+
 .PHONY: kde-baseapps
 kde-baseapps:$(KDEBASEAPPS)_$(ARCH).deb
 $(KDEBASEAPPS): $(SPREZZ)/kde-baseapps/debian/changelog
