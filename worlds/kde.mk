93b09fb8f8452896bcce78d41695949b3799649e
nick black <nick.black@sprezzatech.com>
2012-12-29 07:36:22 -0500

    oxygen is kde

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -13,3 +13,11 @@ $(KDEBASEAPPS): $(SPREZZ)/kde-baseapps/debian/changelog
 	cp -r $(<D) $@/
 	cd $@ && uscan --force-download --download-current-version
 	tar xJvf kde-baseapps_$(kde-baseapps_UPVER).orig.tar.xz $(TARARGS) $@
+
+.PHONY: oxygen-icons
+oxygen-icons:$(OXYGENICONS)_$(ARCH).deb
+$(OXYGENICONS): $(SPREZZ)/oxygen-icons/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf oxygen-icons-$(oxygen-icons_UPVER).tar.xz $(TARARGS) $@
