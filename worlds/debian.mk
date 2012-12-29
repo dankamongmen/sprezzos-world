8bee94e8f270e7a225d11874514911bb2b8e46a7
nick black <nick.black@sprezzatech.com>
2012-12-29 17:23:35 -0500

    add sensible-utils recipe. extract debian native packages to their own world

---
+++ worlds/debian.mk
@@ -0,0 +1,71 @@
+# Native packages (those containing their own source)
+.PHONY: anna
+anna:$(ANNA)_$(ARCH).udeb
+$(ANNA): $(SPREZZ)/anna/debian/changelog
+	cp -r $(<D)/.. $@
+
+.PHONY: base-files
+base-files:$(BASEFILES)_$(ARCH).deb
+$(BASEFILES): $(SPREZZ)/base-files/debian/changelog
+	cp -r $(<D)/.. $@
+	tar cJvf base-files_$(base-files_UPVER).orig.tar.xz $@ --exclude-vcs --exclude=debian
+
+.PHONY: cdebconf
+cdebconf:$(CDEBCONF)_$(ARCH).deb
+$(CDEBCONF): $(SPREZZ)/cdebconf/debian/changelog
+	cp -r $(<D)/.. $@
+	tar cJvf cdebconf_$(cdebconf_UPVER).orig.tar.xz $@ --exclude-vcs --exclude=debian
+
+.PHONY: devscripts
+devscripts:$(DEVSCRIPTS)_$(ARCH).deb
+$(DEVSCRIPTS): $(SPREZZ)/devscripts/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf devscripts_$(devscripts_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
+.PHONY: dpkg
+dpkg:$(DPKG)_$(ARCH).deb
+$(DPKG): $(SPREZZ)/dpkg/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf dpkg_$(dpkg_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
+.PHONY: meta-gnome
+meta-gnome:$(METAGNOME)_$(ARCH).deb
+$(METAGNOME): $(SPREZZ)/meta-gnome/debian/changelog
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf meta-gnome_$(meta-gnome_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
+.PHONY: python-central
+python-central:$(PYTHONCENTRAL)_$(ARCH).deb
+$(PYTHONCENTRAL): $(SPREZZ)/python-central/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf python-central_$(python-central_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
+.PHONY: python-support
+python-support:$(PYTHONSUPPORT)_$(ARCH).deb
+$(PYTHONSUPPORT): $(SPREZZ)/python-support/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf python-support_$(python-support_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
+.PHONY: sensible-utils
+sensible-utils:$(SENSIBLEUTILS)_$(ARCH).deb
+$(SENSIBLEUTILS): $(SPREZZ)/sensible-utils/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf sensible-utils_$(sensible-utils_UPVER).orig.tar.xz $@ --exclude-vcs
+	cp -r $(<D) $@
+
