977b4d066b6186967964103e737cd26c89a8306a
nick black <nick.black@sprezzatech.com>
2012-12-30 18:03:32 -0500

    add kde-workspace

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -1,3 +1,11 @@
+.PHONY: filelight
+filelight:$(filelight)_$(ARCH).deb
+$(filelight): $(SPREZZ)/filelight/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf filelight_$(filelight_UPVER).orig.tar.xz $(TARARGS) $@
+
 .PHONY: kdeartwork
 kdeartwork:$(KDEARTWORK)_$(ARCH).deb
 $(KDEARTWORK): $(SPREZZ)/kdeartwork/debian/changelog
@@ -13,6 +21,14 @@ $(KDEBASEAPPS): $(SPREZZ)/kde-baseapps/debian/changelog
 	cp -r $(<D) $@/
 	cd $@ && uscan --force-download --download-current-version
 	tar xJvf kde-baseapps_$(kde-baseapps_UPVER).orig.tar.xz $(TARARGS) $@
+
+.PHONY: kde-workspace
+kde-workspace:$(kdeworkspace)_$(ARCH).deb
+$(kdeworkspace): $(SPREZZ)/kde-workspace/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf kde-workspace_$(kde-workspace_UPVER).orig.tar.xz $(TARARGS) $@
 
 .PHONY: oxygen-icons
 oxygen-icons:$(OXYGENICONS)_$(ARCH).deb
