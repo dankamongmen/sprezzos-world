146269fcdd6ec1875c43ba1d8ec6065624a0742d
nick black <nick.black@sprezzatech.com>
2012-12-30 18:22:46 -0500

    add recipes for kdepimlibs and kactivities

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -6,6 +6,14 @@ $(filelight): $(SPREZZ)/filelight/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xJvf filelight_$(filelight_UPVER).orig.tar.xz $(TARARGS) $@
 
+.PHONY: kactivities
+kactivities:$(kactivities)_$(ARCH).deb
+$(kactivities): $(SPREZZ)/kactivities/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf kactivities_$(kactivities_UPVER).orig.tar.xz $(TARARGS) $@
+
 .PHONY: kdeartwork
 kdeartwork:$(KDEARTWORK)_$(ARCH).deb
 $(KDEARTWORK): $(SPREZZ)/kdeartwork/debian/changelog
@@ -21,6 +29,14 @@ $(KDEBASEAPPS): $(SPREZZ)/kde-baseapps/debian/changelog
 	cp -r $(<D) $@/
 	cd $@ && uscan --force-download --download-current-version
 	tar xJvf kde-baseapps_$(kde-baseapps_UPVER).orig.tar.xz $(TARARGS) $@
+
+.PHONY: kdepimlibs
+kdepimlibs:$(kdepimlibs)_$(ARCH).deb
+$(kdepimlibs): $(SPREZZ)/kdepimlibs/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xJvf kdepimlibs_$(kdepimlibs_UPVER).orig.tar.xz $(TARARGS) $@
 
 .PHONY: kde-workspace
 kde-workspace:$(kdeworkspace)_$(ARCH).deb
