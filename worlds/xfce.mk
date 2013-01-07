701824ee122e6378120658b334314db7fddc93c5
nick black <nick.black@sprezzatech.com>
2013-01-07 03:19:12 -0500

    add recipe for xfce4-session

--- worlds/xfce.mk
+++ worlds/xfce.mk
@@ -30,6 +30,14 @@ $(LIBXFCE4UTIL): $(SPREZZ)/libxfce4util/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xjvf libxfce4util-$(libxfce4util_UPVER).tar.bz2 $(TARARGS) $@
 
+.PHONY: xfce4-session
+xfce4-session:$(XFCE4SESSION)_$(ARCH).deb
+$(XFCE4SESSION): $(SPREZZ)/xfce4-session/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf xfce4-session-$(xfce4-session_UPVER).tar.bz2 $(TARARGS) $@
+
 .PHONY: xfce4-terminal
 xfce4-terminal:$(XFCE4TERMINAL)_$(ARCH).deb
 $(XFCE4TERMINAL): $(SPREZZ)/xfce4-terminal/debian/changelog
