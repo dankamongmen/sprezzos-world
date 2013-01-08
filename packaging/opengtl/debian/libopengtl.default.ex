1538693db52d0f3aef903071e40616efb82fbc04
nick black <nick.black@sprezzatech.com>
2013-01-08 16:04:59 -0500

    package up opengtl

---
+++ packaging/opengtl/debian/libopengtl.default.ex
@@ -0,0 +1,10 @@
+# Defaults for libopengtl initscript
+# sourced by /etc/init.d/libopengtl
+# installed at /etc/default/libopengtl by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
