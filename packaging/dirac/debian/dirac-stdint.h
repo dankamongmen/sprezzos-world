6aaf5c486596d5296de8b666449a15bda8f03f29
nick black <nick.black@sprezzatech.com>
2013-01-08 17:53:52 -0500

    import and adopt dirac

---
+++ packaging/dirac/debian/dirac-stdint.h
@@ -0,0 +1,8 @@
+#ifndef _DIRAC_LIBDIRAC_COMMON_DIRAC_STDINT_H
+#define _DIRAC_LIBDIRAC_COMMON_DIRAC_STDINT_H 1
+#ifndef _GENERATED_STDINT_H
+#define _GENERATED_STDINT_H "dirac 1.0.2"
+#define _STDINT_HAVE_STDINT_H 1
+#include <stdint.h>
+#endif
+#endif
