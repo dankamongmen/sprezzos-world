ab9469c350088c869212efcd63cb0de1a563484e
nick black <nick.black@sprezzatech.com>
2013-01-25 22:05:24 -0500

    update rygel paths

--- worlds/ubuntu.mk
+++ worlds/ubuntu.mk
@@ -209,7 +209,7 @@ $(UNITY): $(SPREZZ)/unity/debian/changelog
 	bzr branch lp:unity $@
 	rm -rf $@/debian
 	tar cJf unity-$(unity_UPVER).tar.xz $@ --exclude-vcs
-	ln -sf unity-$(unity_UPVER).tar.xz unity-$(unity_UPVER).orig.tar.xz
+	ln -sf unity-$(unity_UPVER).tar.xz unity_$(unity_UPVER).orig.tar.xz
 	cp -r $(<D) $@/
 
 # We're building from bzr in Makefile
