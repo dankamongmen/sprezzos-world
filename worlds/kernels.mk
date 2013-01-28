4406b6d5b752d5cfc476a2ca4c1cac378f08f1e5
nick black <nick.black@sprezzatech.com>
2013-01-28 18:23:41 -0500

    start using linux-support to build our kernels

--- worlds/kernels.mk
+++ worlds/kernels.mk
@@ -1,7 +1,8 @@
 .PHONY: linux
 linux:$(LINUX)_$(ARCH).deb
-$(LINUX): $(SPREZZ)/linux/debian/changelog
+$(LINUX): $(SPREZZ)/linux-support/debian/changelog
 	mkdir $@
 	cp -r $(<D) $@/
 	cd $@ && uscan --force-download --download-current-version
 	tar xJvf linux-$(linux_UPVER).tar.xz $(TARARGS) $@
+	cd $@ && debuild || true
