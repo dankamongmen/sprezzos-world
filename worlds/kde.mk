1003251b043595f75c5791c85884d82acc29c79f
nick black <nick.black@sprezzatech.com>
2013-02-01 18:22:35 -0500

    import, adopt, and upgrade liblastfm 1.03 -> 1.06

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -6,6 +6,14 @@ $(AKONADI): $(SPREZZ)/akonadi/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xjvf akonadi-$(akonadi_UPVER).tar.bz2 $(TARARGS) $@
 
+.PHONY: amarok
+amarok:$(AMAROK)_$(ARCH).deb
+$(AMAROK): $(SPREZZ)/amarok/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf amarok-$(amarok_UPVER).tar.bz2 $(TARARGS) $@
+
 .PHONY: attica
 attica:$(ATTICA)_$(ARCH).deb
 $(ATTICA): $(SPREZZ)/attica/debian/changelog
