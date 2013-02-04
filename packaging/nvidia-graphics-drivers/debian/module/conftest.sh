b60a709d7e855659119e4a2b4f2d98efac56f6e2
nick black <nick.black@sprezzatech.com>
2013-02-04 08:10:57 -0500

    kill duplicate nvidia packaging

--- packaging/nvidia-graphics-drivers/debian/module/conftest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# This is a replacement for nvidia's conftest.sh
-#
-# We have a new conftest.h that works with kbuild that we don't want
-# overwritten or generated or anything else, so we want any call to
-# conftest.sh to do absolutely nothing.
-exit 0
