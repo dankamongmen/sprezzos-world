3918ffa17fe09b09fa7e0991f5d488f830650b28
nick black <nick.black@sprezzatech.com>
2013-02-09 16:02:25 -0500

    fix use of deprecated find -perm +octal (want /octal). this fixes bug 638

---
+++ packaging/debhelper/Debian/Debhelper/Buildsystem/makefile.pm
@@ -0,0 +1,116 @@
+# A debhelper build system class for handling simple Makefile based projects.
+#
+# Copyright: © 2008 Joey Hess
+#            © 2008-2009 Modestas Vainius
+# License: GPL-2+
+
+package Debian::Debhelper::Buildsystem::makefile;
+
+use strict;
+use Debian::Debhelper::Dh_Lib qw(escape_shell clean_jobserver_makeflags);
+use base 'Debian::Debhelper::Buildsystem';
+
+sub exists_make_target {
+	my ($this, $target) = @_;
+
+	# Use make -n to check to see if the target would do
+	# anything. There's no good way to test if a target exists.
+	my @opts=("-s", "-n", "--no-print-directory");
+	my $buildpath = $this->get_buildpath();
+	unshift @opts, "-C", $buildpath if $buildpath ne ".";
+	open(SAVEDERR, ">&STDERR");
+	open(STDERR, ">/dev/null");
+	open(MAKE, "-|", $this->{makecmd}, @opts, $target);
+	my $output=<MAKE>;
+	chomp $output;
+	close MAKE;
+	open(STDERR, ">&SAVEDERR");
+	return defined $output && length $output;
+}
+
+sub do_make {
+	my $this=shift;
+
+	# Avoid possible warnings about unavailable jobserver,
+	# and force make to start a new jobserver.
+	clean_jobserver_makeflags();
+
+	# Note that this will override any -j settings in MAKEFLAGS.
+	unshift @_, "-j" . ($this->get_parallel() > 0 ? $this->get_parallel() : "");
+
+	$this->doit_in_builddir($this->{makecmd}, @_);
+}
+
+sub make_first_existing_target {
+	my $this=shift;
+	my $targets=shift;
+
+	foreach my $target (@$targets) {
+		if ($this->exists_make_target($target)) {
+			$this->do_make($target, @_);
+			return $target;
+		}
+	}
+	return undef;
+}
+
+sub DESCRIPTION {
+	"simple Makefile"
+}
+
+sub new {
+	my $class=shift;
+	my $this=$class->SUPER::new(@_);
+	$this->{makecmd} = (exists $ENV{MAKE}) ? $ENV{MAKE} : "make";
+	return $this;
+}
+
+sub check_auto_buildable {
+	my $this=shift;
+	my ($step) = @_;
+
+	if (-e $this->get_buildpath("Makefile") ||
+	    -e $this->get_buildpath("makefile") ||
+	    -e $this->get_buildpath("GNUmakefile"))
+	{
+		# This is always called in the source directory, but generally
+		# Makefiles are created (or live) in the build directory.
+		return 1;
+	} elsif ($step eq "clean" && defined $this->get_builddir() &&
+	         $this->check_auto_buildable("configure"))
+	{
+		# Assume that the package can be cleaned (i.e. the build directory can
+		# be removed) as long as it is built out-of-source tree and can be
+		# configured. This is useful for derivative buildsystems which
+		# generate Makefiles.
+		return 1;
+	}
+	return 0;
+}
+
+sub build {
+	my $this=shift;
+	$this->do_make(@_);
+}
+
+sub test {
+	my $this=shift;
+	$this->make_first_existing_target(['test', 'check'], @_);
+}
+
+sub install {
+	my $this=shift;
+	my $destdir=shift;
+	$this->make_first_existing_target(['install'],
+		"DESTDIR=$destdir",
+		"AM_UPDATE_INFO_DIR=no", @_);
+}
+
+sub clean {
+	my $this=shift;
+	if (!$this->rmdir_builddir()) {
+		$this->make_first_existing_target(['distclean', 'realclean', 'clean'], @_);
+	}
+}
+
+1
--- worlds/debian.mk
+++ worlds/debian.mk
@@ -93,6 +93,16 @@ $(CONSOLESETUP): $(SPREZZ)/console-setup/debian/changelog
 	cp -r $(<D)/.. $@
 	tar cJvf console-setup_$(console-setup_UPVER).orig.tar.xz $@ --exclude-vcs --exclude=debian
 
+.PHONY: debhelper
+debhelper:$(DEBHELPER)_$(ARCH).deb
+$(DEBHELPER): $(SPREZZ)/debhelper/debian/changelog
+	@[ ! -e $@ ] || { echo "Removing $@..." && rm -rf $@ ; }
+	cp -r $(<D)/.. $@
+	rm -rf $@/debian
+	tar cJvf debhelper-$(debhelper_UPVER).tar.xz $@ --exclude-vcs
+	ln -sf debhelper-$(debhelper_UPVER).tar.xz debhelper_$(debhelper_UPVER).orig.tar.xz
+	cp -r $(<D) $@
+
 .PHONY: debian-cd
 debian-cd:$(DEBIANCD)_$(ARCH).deb
 $(DEBIANCD): $(SPREZZ)/debian-cd/debian/changelog
