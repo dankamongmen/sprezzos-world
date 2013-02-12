8ca38ef3996084a8756b786b7543156739f38c99
nick black <nick.black@sprezzatech.com>
2013-02-11 21:56:17 -0500

    rebuild gimp with working depends list

--- worlds/xapps.mk
+++ worlds/xapps.mk
@@ -4,5 +4,5 @@ $(PIDGINFACEBOOKCHAT): $(SPREZZ)/pidgin-facebookchat/debian/changelog
 	mkdir $@
 	cp -r $(<D) $@
 	cd $@ && uscan --force-download --download-current-version
-	tar xzvf pidgin-facebookchat-$(pidgin-facebookchat_UPVER).tar.gz $(TARARGS) $@
+	tar xjvf pidgin-facebookchat-source-$(pidgin-facebookchat_UPVER).tar.bz2 $(TARARGS) $@
 
