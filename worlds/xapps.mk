62ea6f650eea224fa6ed08a637ac81e9a8d6421e
nick black <nick.black@sprezzatech.com>
2013-02-12 15:22:53 -0500

    add recipe for icoutils

--- worlds/xapps.mk
+++ worlds/xapps.mk
@@ -6,3 +6,11 @@ $(PIDGINFACEBOOKCHAT): $(SPREZZ)/pidgin-facebookchat/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xjvf pidgin-facebookchat-source-$(pidgin-facebookchat_UPVER).tar.bz2 $(TARARGS) $@
 
+.PHONY: icoutils
+icoutils:$(ICOUTILS)_$(ARCH).deb
+$(ICOUTILS): $(SPREZZ)/icoutils/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xjvf icoutils-$(icoutils_UPVER).tar.bz2 $(TARARGS) $@
+
