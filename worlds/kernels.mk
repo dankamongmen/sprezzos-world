11dd6c3e991675717431ba952764252bc3416d47
nick black <nick.black@sprezzatech.com>
2013-02-13 12:01:13 -0500

    import curl file from debian

--- worlds/kernels.mk
+++ worlds/kernels.mk
@@ -1,6 +1,6 @@
 .PHONY: linux
 linux:$(LINUX)_$(ARCH).deb
-$(LINUX): $(SPREZZ)/linux-support/debian/changelog
+$(LINUX): $(SPREZZ)/linux/debian/changelog
 	mkdir $@
 	cp -r $(<D) $@/
 	cd $@ && uscan --force-download --download-current-version
