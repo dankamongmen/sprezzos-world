3021067f31e64a872266053cf195b2e575a24f1b
nick black <nick.black@sprezzatech.com>
2013-03-01 12:12:33 -0500

    add jansi recipe

--- worlds/java.mk
+++ worlds/java.mk
@@ -6,3 +6,11 @@ $(RHINO): $(SPREZZ)/rhino/debian/changelog
 	cd $@ && uscan --force-download --download-current-version --repack
 	tar xzvf rhino_$(rhino_UPVER).orig.tar.gz $(TARARGS) $@
 
+.PHONY: jansi
+jansi:$(JANSI)_$(ARCH).deb
+$(JANSI): $(SPREZZ)/jansi/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf jansi-$(jansi_UPVER).tar.gz $(TARARGS) $@
+
