929358e086360eb9f4e161a3907b572565e8d489
nick black <nick.black@sprezzatech.com>
2013-03-06 09:36:39 -0500

    import, adopt, and upgrade ocaml-ogg

--- worlds/ml.mk
+++ worlds/ml.mk
@@ -6,3 +6,11 @@ $(FINDLIB): $(SPREZZ)/findlib/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xzvf findlib-$(findlib_UPVER).tar.gz $(TARARGS) $@
 
+.PHONY: ocaml-ogg
+ocaml-ogg:$(OCAMLOGG)_$(ARCH).deb
+$(OCAMLOGG): $(SPREZZ)/ocaml-ogg/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-ogg-$(ocaml-ogg_UPVER).tar.gz $(TARARGS) $@
+
