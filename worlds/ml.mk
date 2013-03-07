af20c6e2bb26b296803d9eec7ae34511cade169f
nick black <nick.black@sprezzatech.com>
2013-03-07 18:59:40 -0500

    drop both gprolog patches

--- worlds/ml.mk
+++ worlds/ml.mk
@@ -62,3 +62,219 @@ $(OCAMLBITSTRING): $(SPREZZ)/ocaml-bitstring/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xzvf ocaml-bitstring-$(ocaml-bitstring_UPVER).tar.gz $(TARARGS) $@
 
+.PHONY: ocaml-cry
+ocaml-cry:$(OCAMLCRY)_$(ARCH).deb
+$(OCAMLCRY): $(SPREZZ)/ocaml-cry/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-cry_$(ocaml-cry_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-vorbis
+ocaml-vorbis:$(OCAMLVORBIS)_$(ARCH).deb
+$(OCAMLVORBIS): $(SPREZZ)/ocaml-vorbis/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-vorbis_$(ocaml-vorbis_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-mad
+ocaml-mad:$(OCAMLMAD)_$(ARCH).deb
+$(OCAMLMAD): $(SPREZZ)/ocaml-mad/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-mad_$(ocaml-mad_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-dtools
+ocaml-dtools:$(OCAMLDTOOLS)_$(ARCH).deb
+$(OCAMLDTOOLS): $(SPREZZ)/ocaml-dtools/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-dtools_$(ocaml-dtools_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-taglib
+ocaml-taglib:$(OCAMLTAGLIB)_$(ARCH).deb
+$(OCAMLTAGLIB): $(SPREZZ)/ocaml-taglib/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-taglib_$(ocaml-taglib_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-ao
+ocaml-ao:$(OCAMLAO)_$(ARCH).deb
+$(OCAMLAO): $(SPREZZ)/ocaml-ao/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-ao_$(ocaml-ao_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-duppy
+ocaml-duppy:$(OCAMLDUPPY)_$(ARCH).deb
+$(OCAMLDUPPY): $(SPREZZ)/ocaml-duppy/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-duppy_$(ocaml-duppy_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-alsa
+ocaml-alsa:$(OCAMLALSA)_$(ARCH).deb
+$(OCAMLALSA): $(SPREZZ)/ocaml-alsa/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-alsa_$(ocaml-alsa_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-ladspa
+ocaml-ladspa:$(OCAMLLADSPA)_$(ARCH).deb
+$(OCAMLLADSPA): $(SPREZZ)/ocaml-ladspa/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-ladspa_$(ocaml-ladspa_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-soundtouch
+ocaml-soundtouch:$(OCAMLSOUNDTOUCH)_$(ARCH).deb
+$(OCAMLSOUNDTOUCH): $(SPREZZ)/ocaml-soundtouch/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-soundtouch_$(ocaml-soundtouch_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-lastfm
+ocaml-lastfm:$(OCAMLLASTFM)_$(ARCH).deb
+$(OCAMLLASTFM): $(SPREZZ)/ocaml-lastfm/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-lastfm_$(ocaml-lastfm_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-speex
+ocaml-speex:$(OCAMLSPEEX)_$(ARCH).deb
+$(OCAMLSPEEX): $(SPREZZ)/ocaml-speex/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-speex_$(ocaml-speex_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-gavl
+ocaml-gavl:$(OCAMLGAVL)_$(ARCH).deb
+$(OCAMLGAVL): $(SPREZZ)/ocaml-gavl/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-gavl_$(ocaml-gavl_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-theora
+ocaml-theora:$(OCAMLTHEORA)_$(ARCH).deb
+$(OCAMLTHEORA): $(SPREZZ)/ocaml-theora/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-theora_$(ocaml-theora_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-portaudio
+ocaml-portaudio:$(OCAMLPORTAUDIO)_$(ARCH).deb
+$(OCAMLPORTAUDIO): $(SPREZZ)/ocaml-portaudio/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-portaudio_$(ocaml-portaudio_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-samplerate
+ocaml-samplerate:$(OCAMLSAMPLERATE)_$(ARCH).deb
+$(OCAMLSAMPLERATE): $(SPREZZ)/ocaml-samplerate/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-samplerate_$(ocaml-samplerate_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-xmlplaylist
+ocaml-xmlplaylist:$(OCAMLXMLPLAYLIST)_$(ARCH).deb
+$(OCAMLXMLPLAYLIST): $(SPREZZ)/ocaml-xmlplaylist/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-xmlplaylist_$(ocaml-xmlplaylist_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-magic
+ocaml-magic:$(OCAMLMAGIC)_$(ARCH).deb
+$(OCAMLMAGIC): $(SPREZZ)/ocaml-magic/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-magic_$(ocaml-magic_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-bjack
+ocaml-bjack:$(OCAMLBJACK)_$(ARCH).deb
+$(OCAMLBJACK): $(SPREZZ)/ocaml-bjack/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-bjack_$(ocaml-bjack_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-faad
+ocaml-faad:$(OCAMLFAAD)_$(ARCH).deb
+$(OCAMLFAAD): $(SPREZZ)/ocaml-faad/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-faad_$(ocaml-faad_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-mm
+ocaml-mm:$(OCAMLMM)_$(ARCH).deb
+$(OCAMLMM): $(SPREZZ)/ocaml-mm/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-mm_$(ocaml-mm_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-flac
+ocaml-flac:$(OCAMLFLAC)_$(ARCH).deb
+$(OCAMLFLAC): $(SPREZZ)/ocaml-flac/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-flac_$(ocaml-flac_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-schroedinger
+ocaml-schroedinger:$(OCAMLSCHROEDINGER)_$(ARCH).deb
+$(OCAMLSCHROEDINGER): $(SPREZZ)/ocaml-schroedinger/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-schroedinger_$(ocaml-schroedinger_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-voaacenc
+ocaml-voaacenc:$(OCAMLVOAACENC)_$(ARCH).deb
+$(OCAMLVOAACENC): $(SPREZZ)/ocaml-voaacenc/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-voaacenc_$(ocaml-voaacenc_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-dssi
+ocaml-dssi:$(OCAMLDSSI)_$(ARCH).deb
+$(OCAMLDSSI): $(SPREZZ)/ocaml-dssi/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-dssi_$(ocaml-dssi_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-lo
+ocaml-lo:$(OCAMLLO)_$(ARCH).deb
+$(OCAMLLO): $(SPREZZ)/ocaml-lo/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-lo_$(ocaml-lo_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ocaml-gstreamer
+ocaml-gstreamer:$(OCAMLGSTREAMER)_$(ARCH).deb
+$(OCAMLGSTREAMER): $(SPREZZ)/ocaml-gstreamer/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ocaml-gstreamer_$(ocaml-gstreamer_UPVER).orig.tar.gz $(TARARGS) $@
+
