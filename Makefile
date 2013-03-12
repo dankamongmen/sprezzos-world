579c850362b58fab1d1b11c76f3fd2e6f4c512fe
nick black <nick.black@sprezzatech.com>
2013-03-12 14:08:25 -0400

    import, adopt, and upgrade ethtool -> 3.8

--- Makefile
+++ Makefile
@@ -9944,3 +9944,19 @@ $(SRATOM): $(SPREZZ)/sratom/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xjvf sratom_$(sratom_UPVER).orig.tar.bz2 $(TARARGS) $@
 
+.PHONY: bridge-utils
+bridge-utils:$(BRIDGEUTILS)_$(ARCH).deb
+$(BRIDGEUTILS): $(SPREZZ)/bridge-utils/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf bridge-utils_$(bridge-utils_UPVER).orig.tar.gz $(TARARGS) $@
+
+.PHONY: ethtool
+ethtool:$(ETHTOOL)_$(ARCH).deb
+$(ETHTOOL): $(SPREZZ)/ethtool/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf ethtool_$(ethtool_UPVER).orig.tar.gz $(TARARGS) $@
+
---
+++ packaging/bridge-utils/debian/bridge-network-interface.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# bridge-network-interface - configure a network bridge
+#
+# This service checks whether a physical network device that has been added
+# is one of the ports in a bridge config, and if so, brings up the related
+# bridge
+
+set -e
+
+if [ -z "$INTERFACE" ]; then
+	echo "missing \$INTERFACE" >&2
+	exit 1
+fi
+
+#default configuration
+BRIDGE_HOTPLUG=no
+[ -f /etc/default/bridge-utils ] && . /etc/default/bridge-utils
+
+[ "$BRIDGE_HOTPLUG" = "no" ] && exit 0
+
+. /lib/bridge-utils/bridge-utils.sh
+
+if [ -d /run/network ]; then
+   for i in $(ifquery --list --allow auto); do
+	ports=$(ifquery $i | sed -n -e's/^bridge[_-]ports: //p')
+	for port in $(bridge_parse_ports $ports); do
+		case $port in
+			$INTERFACE|$INTERFACE.*)
+				if [ ! -d /sys/class/net/$port ] &&
+				   [ -x /etc/network/if-pre-up.d/vlan ]; then
+					IFACE=$port /etc/network/if-pre-up.d/vlan
+				fi
+
+				if [ -d /sys/class/net/$port ]; then
+					ifup --allow auto $i
+					brctl addif $i $port && ifconfig $port 0.0.0.0 up
+				fi
+				break
+				;;
+		esac
+	done
+   done
+fi
