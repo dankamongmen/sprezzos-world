3e2308c4e10ded6e412eab59f029016594432423
Nick Black <nick.black@sprezzatech.com>
2013-04-22 09:37:22 -0400

    package adobe's sourcecodepro font

--- worlds/ml.mk
+++ worlds/ml.mk
@@ -454,11 +454,11 @@ $(OCAMLRE): $(SPREZZ)/ocaml-re/debian/changelog
 	cd $@ && uscan --force-download --download-current-version
 	tar xzvf ocaml-re_$(ocaml-re_UPVER).orig.tar.gz $(TARARGS) $@
 
-.PHONY: libfrei0r-ocaml
-libfrei0r-ocaml:$(LIBFREI0ROCAML)_$(ARCH).deb
-$(LIBFREI0ROCAML): $(SPREZZ)/libfrei0r-ocaml/debian/changelog
+.PHONY: ocaml-frei0r
+ocaml-frei0r:$(OCAMLFREI0R)_$(ARCH).deb
+$(OCAMLFREI0R): $(SPREZZ)/ocaml-frei0r/debian/changelog
 	mkdir $@
 	cp -r $(<D) $@
 	cd $@ && uscan --force-download --download-current-version
-	tar xzvf libfrei0r-ocaml_$(libfrei0r-ocaml_UPVER).orig.tar.gz $(TARARGS) $@
+	tar xzvf ocaml-frei0r_$(ocaml-frei0r_UPVER).orig.tar.gz $(TARARGS) $@
 
