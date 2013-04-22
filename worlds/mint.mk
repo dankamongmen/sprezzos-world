a400ad358b58e455c0ce8da242a16ffd1b45a682
Nick Black <nick.black@sprezzatech.com>
2013-04-21 22:08:46 -0400

    return to mainline muffin -> 1.7.3

--- worlds/mint.mk
+++ worlds/mint.mk
@@ -7,24 +7,24 @@ $(MINTSYSTEM): $(SPREZZ)/mintsystem/debian/changelog
 	ln -sf mintsystem-$(mintsystem_UPVER).tar.xz mintsystem_$(mintsystem_UPVER).orig.tar.xz
 	cp -r $(<D) $@/
 
-.PHONY: muffin
-muffin:$(MUFFIN)_$(ARCH).deb
-$(MUFFIN): $(SPREZZ)/muffin/debian/changelog
-	git clone git@github.com:dankamongmen/muffin.git $@
-	rm -rf $@/debian
-	tar cJf muffin-$(muffin_UPVER).tar.xz $@ --exclude-vcs
-	ln -sf muffin-$(muffin_UPVER).tar.xz muffin_$(muffin_UPVER).orig.tar.xz
-	cp -r $(<D) $@/
-
 #.PHONY: muffin
 #muffin:$(MUFFIN)_$(ARCH).deb
 #$(MUFFIN): $(SPREZZ)/muffin/debian/changelog
-#	mkdir $@
-#	cp -r $(<D) $@/
-#	cd $@ && uscan --force-download --download-current-version
-#	tar xzvf muffin_$(muffin_UPVER).orig.tar.gz $(TARARGS) $@
+#	git clone git@github.com:dankamongmen/muffin.git $@
 #	rm -rf $@/debian
+#	tar cJf muffin-$(muffin_UPVER).tar.xz $@ --exclude-vcs
+#	ln -sf muffin-$(muffin_UPVER).tar.xz muffin_$(muffin_UPVER).orig.tar.xz
 #	cp -r $(<D) $@/
+
+.PHONY: muffin
+muffin:$(MUFFIN)_$(ARCH).deb
+$(MUFFIN): $(SPREZZ)/muffin/debian/changelog
+	mkdir $@
+	cp -r $(<D) $@/
+	cd $@ && uscan --force-download --download-current-version
+	tar xzvf muffin_$(muffin_UPVER).orig.tar.gz $(TARARGS) $@
+	rm -rf $@/debian
+	cp -r $(<D) $@/
 
 .PHONY: cinnamon
 cinnamon:$(CINNAMON)_$(ARCH).deb
