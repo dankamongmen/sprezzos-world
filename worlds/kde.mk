ef72ef3bde5338c3eb34990b14b340f5ca4c35d0
Nick Black <nick.black@sprezzatech.com>
2013-05-23 13:57:26 -0400

    digikam wants qtgstreamer

--- worlds/kde.mk
+++ worlds/kde.mk
@@ -668,5 +668,5 @@ $(QTGSTREAMER): $(SPREZZ)/qt-gstreamer/debian/changelog
 	mkdir $@
 	cp -r $(<D) $@
 	cd $@ && uscan --force-download --download-current-version
-	tar xzvf qt-gstreamer_$(qt-gstreamer_UPVER).orig.tar.gz $(TARARGS) $@
+	tar xjvf qt-gstreamer_$(qt-gstreamer_UPVER).orig.tar.bz2 $(TARARGS) $@
 
